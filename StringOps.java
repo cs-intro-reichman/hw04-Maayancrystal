3b80eb89a25102ba79b1e7879365781ea517f129
Maayan <maayannehama.crystal@post.runi.ac.il>
2024-01-12 08:50:15 +0200

    friday

--- StringOps.java
+++ StringOps.java
@@ -22,12 +22,33 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        String string = "yellow";
+        System.out.println(capVowelsLowRest(string)); // “hEllO wOrld”;
+        //System.out.println(capVowlesLowRest(“One two tHRee world”)); // “OnE twO thrEE wOrld”;
+        //System.out.println(capVowlesLowRest(“vowels are fun”)); // “vOwEls ArE fUn”;
+        //System.out.println(capVowlesLowRest(“intro”)); // “IntrO”;
+        //System.out.println(capVowelsLowRest(“yellow”)); // “yEllOw”;
+          
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        String newString = "";
+        for (int i = 0 ; i < string.length() ; i++){
+            char ch = string.charAt(i);
+            if(isVowel(ch) == true ){
+                ch = (char)(ch - 32);
+            }
+            else{
+                if (ch >= 'A' && ch <= 'Z'){
+                    ch = (char)(ch + 32);
+                  }
+                  else{
+                    ch = (char)(ch + 0);
+                  }
+            }
+            newString = newString + ch;
+        }
+        return newString;
     }
 
     public static String camelCase (String string) {
@@ -38,5 +59,28 @@ public class StringOps {
     public static int[] allIndexOf (String string, char chr) {
         // Write your code here:
         return new int[1];
+    }
+
+    public static boolean isVowel (char ch) {
+        boolean isVowel = false;
+        switch (ch) {
+            case 'a': 
+                isVowel = true;
+                break;
+            case 'e': 
+                isVowel = true;
+                break;   
+            case 'i': 
+                isVowel = true;
+                break; 
+            case 'o': 
+                isVowel = true;
+                break; 
+            case 'u': 
+                isVowel = true;
+                break;
+        
+        }
+        return isVowel;
     }
 }
